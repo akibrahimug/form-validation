508d24335825ffc1003948f88d537aa6bee01643
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-01-10 15:57:09 +0000

    Finalize the form validation function

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const userName = document.querySelector('#name');
 
 // Add a focus state on the name input field
 userName.focus();
+
 //===========================================
 //Hide the Other input field on Job Role
 //===========================================
@@ -123,7 +124,7 @@ const totalCost = () => {
     //Iterate through all checkboxes and listen for changes
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', e => {
-            
+
             // if a checkbox is checked
             if(e.currentTarget.checked){
                 //Assign the data-cost attribute to the 'itemCost' and ParseInt to turn a string into a number
@@ -133,8 +134,17 @@ const totalCost = () => {
                 count += itemCost;
 
                 //Modify the total inner html to reflect the addition
-                cost.innerHTML = `Total: $${count}`
-                
+                cost.innerHTML = `Total: $${count}`;
+
+                 //Remove the 'not-valid' class from the activities section
+                activities.classList.remove('not-valid');
+
+                // Add 'valid' class to the activities section
+                activities.classList.add('valid');
+
+                // Hide the hint message 
+                activities.lastElementChild.style.display = 'none'
+
                 //if a checkbox is unchecked
             }else{
 
@@ -143,14 +153,51 @@ const totalCost = () => {
 
                 //Subtract the itemCost to the count
                 count -= itemCost
-
-                //Modify the total inner html to reflect the subtraction
-                cost.innerHTML = `Total: $${count}`
                 
+                //Modify the total inner html to reflect the subtraction
+                cost.innerHTML = `Total: $${count}`;
+
+                if(count <= 0){
+
+                     // Add 'not-valid' class to the activities section
+                    activities.classList.add('not-valid');
+
+                    // Display a hint message on how to correct the error
+                    activities.lastElementChild.style.display = 'block';
+                }
             }
         })
     })
 }
+
+//================================================================
+// Focus and blur events on the register for activities checkboxes
+//================================================================
+
+// Iterate through the checkboxes
+checkboxes.forEach(checkbox => {
+    //Add an event listener for the focus event
+    checkbox.addEventListener('focus', e => {
+        //If any of the checkboxes is in focus
+        if(e.currentTarget){
+            // Add the 'focus' class to the parent its contained under
+            checkbox.parentElement.classList.add('focus')
+        }
+    })
+    // Add an event listener for the blur event
+    checkbox.addEventListener('blur', e => {
+        // If any checkboxes are in blur
+        if(e.currentTarget){
+            // Remove the 'focus' class from its parent element
+            checkbox.parentElement.classList.remove('focus')
+        }
+    })
+})
+
+
+//=============================================
+// Select credit card payment method by default
+// ============================================
 
  //Assign the paypal option info to the variable 'paypal'
  const paypal = document.querySelector('#paypal');
@@ -161,10 +208,6 @@ const totalCost = () => {
  const bitcoin = document.querySelector('#bitcoin');
  //make bitcoin info hidden
  bitcoin.style.display = 'none'
-
-//=============================================
-// Select credit card payment method by default
-// ============================================
 
 //Assign the paymet card input field to the variable 'paymentCard'
 const paymentCard = document.querySelector('#payment');
@@ -202,6 +245,11 @@ const paymentInfo = () => {
             // make bitcoin info hidden
             bitcoin.style.display = 'none'
 
+            // make card details hidden
+            cardNumber.parentElement.style.display = 'none';
+            zipCode.parentElement.style.display = 'none';
+            cvv.parentElement.style.display = 'none'
+
             //if value is credit-card
         }else if(e.target.value === 'credit-card'){
 
@@ -211,6 +259,11 @@ const paymentInfo = () => {
             // make bitcoin info hidden
             bitcoin.style.display = 'none'
 
+            // make card details shown
+            cardNumber.parentElement.style.display = 'block';
+            zipCode.parentElement.style.display = 'block';
+            cvv.parentElement.style.display = 'block'
+
             //if value is bitcoin
         }else if(e.target.value === 'bitcoin'){
 
@@ -219,6 +272,11 @@ const paymentInfo = () => {
 
             // make paypal info hidden
             paypal.style.display = 'none'
+
+             // make card details hidden
+             cardNumber.parentElement.style.display = 'none';
+             zipCode.parentElement.style.display = 'none';
+             cvv.parentElement.style.display = 'none'
         }
     })
 
@@ -227,23 +285,64 @@ const paymentInfo = () => {
 //Form Validation
 //=================
 
+// Automatic update of error messages as fields are being filled
+const errorMsg = (valid, parent, value) => {
+    // Listen for key strockes 
+    value.addEventListener('keyup', e => {
+        if(valid.test(value.value) !== true){
+            // Add the 'not-valid' class to the parent containing the empty field
+            value.parentElement.classList.add('not-valid');        
+
+            // Display a hint message on how to correct the error
+            parent.lastElementChild.style.display = 'block'
+        }else{
+            // Remove the 'not-valid' class from the parent element
+            value.parentElement.classList.remove('not-valid');
+
+            // Add the 'valid' class to the parent element
+            value.parentElement.classList.add('valid')
+
+            // Hide the hint message 
+            parent.lastElementChild.style.display = 'none'
+        }
+    })
+}
+
 // Assign the email input field to the varaiable 'email'
 const email = document.querySelector('#email');
+
+// Error Message for email
+errorMsg(/^(\w)+@(\w)+\.com$/, email.parentElement, email)
+
+// Error Message for Username
+errorMsg(/^[(A-Z)?a-z]+\s[(A-Z)?a-z]+$/, userName.parentElement, userName)
 
 // Assign the card number input field to the variable 'cardNumber'
 const cardNumber = document.querySelector('#cc-num');
 
+// Error Message for CardNumber
+errorMsg(/^\d{13,16}$/, cardNumber.parentElement, cardNumber)
+
 // Assign the zip code input field to the variable 'zipCode'
 const zipCode = document.querySelector('#zip');
+
+// Error Message for ZipCode
+errorMsg(/^\d{5}$/, zipCode.parentElement, zipCode)
 
 // Assign the cvv input field to the variable 'cvv'
 const cvv = document.querySelector('#cvv');
 
+// Error Message for Cvv
+errorMsg(/^\d{3}$/, cvv.parentElement, cvv)
+
 // Assign the form html element to the variable 'form'
 const form = document.querySelector('form');
 
-// Helper function handling the regex test for all input fields
-const input = (inputTest, value, event) => {
+// Helper function handling the regex test before submit
+const input = (inputTest, value, event, field) => {
+
+     // Assign the parent element to the variable 'parent'
+     const parent = field.parentElement;
 
      // Test the value 
      inputTest.test(value);
@@ -251,8 +350,31 @@ const input = (inputTest, value, event) => {
      if(inputTest.test(value) !== true ){
          //Prevent default submit behaviour
          event.preventDefault();
+
+         // ===================
+         // Custom form errors
+         // ===================
+
+        // Add the 'not-valid' class to the parent containing the empty field
+        field.parentElement.classList.add('not-valid');
+
+        // Display a hint message on how to correct the error
+        parent.lastElementChild.style.display = 'block'
+    
+     }else{
+        // Remove the 'not-valid' class from the parent element
+        field.parentElement.classList.remove('not-valid');
+
+        // Add the 'valid' class to the parent element
+        field.parentElement.classList.add('valid')
+
+        // Hide the hint message 
+        parent.lastElementChild.style.display = 'none'
      }
 }
+
+// Assign the fieldset containing all the activities to the 'activities' variable
+const activities = document.querySelector('#activities');
 
 // Create a function 'activity' to handle a selection from the register for activities section
 const activity = (event) => {
@@ -261,9 +383,27 @@ const activity = (event) => {
     if(count === 0 ){
 
         // Prevent submit default behaviour
-        event.preventDefault()
+        event.preventDefault();
+
+        // Add 'not-valid' class to the activities section
+        activities.classList.add('not-valid');
+
+        // Display a hint message on how to correct the error
+        activities.lastElementChild.style.display = 'block'
+
+    }else{
+
+        //Remove the 'not-valid' class from the activities section
+        activities.classList.remove('not-valid');
+
+        // Add 'valid' class to the activities section
+        activities.classList.add('valid');
+
+        // Hide the hint message 
+        activities.lastElementChild.style.display = 'none'
     }
 }
+
 
 //Create a function to handle credit-card validation
 const creditCard = (event) => {
@@ -274,11 +414,11 @@ const creditCard = (event) => {
             //if option's attribute is 'selected'
             if(option.getAttribute('selected') ){
             //cardNumber
-            input(/^\d{13,16}$/, cardNumber.value, event);
+            input(/^\d{13,16}$/, cardNumber.value, event, cardNumber);
             // zipCode
-            input(/^\d{5}$/, zipCode.value, event);
+            input(/^\d{5}$/, zipCode.value, event, zipCode);
             // cvv
-            input(/^\d{3}$/, cvv.value, event);
+            input(/^\d{3}$/, cvv.value, event, cvv);
             }
         }
     })
@@ -287,15 +427,21 @@ const creditCard = (event) => {
 // Listen for form changes before submit and make sure all fields are properly filled
 form.addEventListener('submit', e => {
     // name
-    input(/^[(A-Z)?a-z]+\s[(A-Z)?a-z]+$/, userName.value, e);
+    input(/^[(A-Z)?a-z]+\s[(A-Z)?a-z]+$/, userName.value, e, userName);
     // email
-    input(/^(\w)+@(\w)+\.com$/, email.value, e);
+    input(/^(\w)+@(\w)+\.com$/, email.value, e, email);
     // activity selection
     activity(e)
     // creditCard
-    creditCard(e);
+    if(paymentCard.value === 'credit-card'){
+        creditCard(e);
+    }
+    
    
 })
+
+
+
 
 // ================
 //Call functions
